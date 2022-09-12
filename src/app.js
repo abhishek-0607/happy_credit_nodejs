664295b20d8ece157e3ca227fa284ed78d04b2e1
Abhishek Silawat <absilawat@gmail.com>
2022-09-13 00:35:13 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const app = express();
+
+const postController = require("./controllers/postController");
 app.use(express.json());
+
+app.use("/post", postController);
 
 module.exports = app;
