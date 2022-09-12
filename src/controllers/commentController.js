f5f7fca31fb77f78746cdcb1ae96e07c0eae71da
Abhishek Silawat <absilawat@gmail.com>
2022-09-13 00:35:16 +0530

    Create commentController.js

---
+++ src/controllers/commentController.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+const Comment = require("../models/comment");
+const Post = require("../models/post");
+
+// router.post("/:id", async (req, res) => {});
+
+module.exports = router;
