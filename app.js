585fcb2bb33831869db8c797216b56fa8e2b58ab
Abhishek Silawat <absilawat@gmail.com>
2022-09-14 17:04:32 +0530

    require changes to deploy, didn't change the logics

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const app = require("./app");
-const connect = require("./configs/db");
+const app = require("./src/app");
+const connect = require("./src/configs/db");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 
