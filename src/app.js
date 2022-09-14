5d6ed6d02d194278ed2c57f9b5a4d84e8ba6f965
Abhishek Silawat <absilawat@gmail.com>
2022-09-14 17:24:45 +0530

    required changes for deploy

--- src/app.js
+++ src/app.js
@@ -1,8 +1,13 @@
 const express = require("express");
+const bodyparser = require("body-parser");
+const cors = require("cors");
 const app = express();
 
 const postController = require("./controllers/postController");
 app.use(express.json());
+
+app.use(bodyparser.json());
+app.use(cors());
 
 app.use("/post", postController);
 
